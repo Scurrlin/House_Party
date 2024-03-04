af76f70577b5d5affb7aec2b06b5f95173386990
Sean Currlin <sean.currlin@gmail.com>
2024-03-04 18:51:46 -0500

    update Room.js

--- house_party/frontend/src/components/Room.js
+++ house_party/frontend/src/components/Room.js
@@ -26,7 +26,7 @@ export default class Room extends Component {
   }
 
   componentDidMount() {
-    this.interval = setInterval(this.getCurrentSong, 1000);
+    this.interval = setInterval(this.getCurrentSong, 10000);
   }
 
   componentWillUnmount() {
